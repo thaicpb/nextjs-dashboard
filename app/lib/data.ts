e47f4c090383451cd4eb2b16ccf188ed578365a5
prNguyenVietThai <thai.nguyenviet@prtimes.co.jp>
2023-11-04 22:14:48 +0900

    update pagination

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -91,7 +91,7 @@ export async function fetchCardData() {
   }
 }
 
-const ITEMS_PER_PAGE = 10;
+const ITEMS_PER_PAGE = 5;
 export async function fetchFilteredInvoices(
   query: string,
   currentPage: number,
